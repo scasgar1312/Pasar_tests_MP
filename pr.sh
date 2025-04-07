ca10cecd1e98ea6625a9d5193e01ad946b26df34
scasgar1312 <201811349+scasgar1312@users.noreply.github.com>
2025-04-08 01:20:50 +0200

    Elimino un script que había creado de prueba.

--- pr.sh
+++
@@ -1,4 +0,0 @@
-
-argumentos='< data/crimes2_bis.b1in'
-
-cd /home/tetonala1312/Escritorio/sasa && valgrind --track-origins=yes --leak-check=full --log-file=$HOME/Escritorio/basura/resultado_valgrind_1.txt "/home/tetonala1312/Escritorio/sasa/dist/Debug/GNU-Linux/boston1" $argumentos
